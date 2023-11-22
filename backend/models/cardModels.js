95a1a09fb849612a103b0ebf037d1c82974e6a29
Christopher Lefrancois <chris.lefrancois1@gmail.com>
2023-11-22 15:07:13 -0500

    created the card model

---
+++ backend/models/cardModels.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+// Schema
+const Schema = mongoose.Schema
+
+const cardSchema = new Schema({
+  title: {type: String, required: true},
+  price: {type: Number, required: true},
+  graded: {type: String, enum: ['Graded', 'Raw'], required: true},
+  category: {type: String, required: true}
+});
+
+// model
+
+export const Card = mongoose.model("Card", cardSchema)
